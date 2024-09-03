755e5f5283ced833be94b1295a46a2c64c3abe8a
HEun0420 <usialeta@gmail.com>
2024-09-03 14:25:19 +0900

    restapi

--- 14_REST-API-lecture-source/src/main/java/com/ohgiraffers/restapi/section05/swagger/SwaggerTestController.java
+++ 14_REST-API-lecture-source/src/main/java/com/ohgiraffers/restapi/section05/swagger/SwaggerTestController.java
@@ -1,5 +1,10 @@
 package com.ohgiraffers.restapi.section05.swagger;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Tag(name="Spring boot swagger 연동 API(User가능)")
 @RestController
 @RequestMapping("/swagger")
 public class SwaggerTestController {
@@ -30,6 +36,7 @@ public class SwaggerTestController {
     }
 
     // user 정보 전체조회
+    @Operation(summary = "전체회원 조회", description = " 우리 사이트의 전체 회원 목록 조회")
     @GetMapping("/users")
     public ResponseEntity<ResponseMessage> findAllUsers() {
 
@@ -53,6 +60,11 @@ public class SwaggerTestController {
 
         return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "회원번호로 회원 조회", description = "회원번호를 통해 회원을 조회한다.",
+            parameters = {@Parameter(name = "userNo", description = "사용자 화면에서 넘어오는 user의 pk")}
+            )
 
     @GetMapping("/users/{userNo}")
     public ResponseEntity<ResponseMessage> findUserByNo(@PathVariable int userNo) {
@@ -109,6 +121,11 @@ public class SwaggerTestController {
     }
 
     // 유저 삭제
+    @Operation(summary = "회원 정보 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "회원삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못 입력된 파라미터")
+    })
     @DeleteMapping("/users/{userNo}")
     public ResponseEntity<?> removeUser(@PathVariable int userNo) {
 
